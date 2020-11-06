62dc569c83e41b204a57830dc2983cde3a425dab
Mounika <mounika@archeplay.com>
2020-11-06 06:01:05 +0000

    maven sample

---
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -0,0 +1,18 @@
+package com.in28minutes.maven;
+
+/**
+ * Hello World test!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World  Test Test123  Test Test Test" );
+    }
+
+	public int calculateSomething()
+	{
+		return 0;
+	}
+}
